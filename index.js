6560a304244d2332e9dc914e75b124ee227f5502
Sam Spilsbury <smspillaz@gmail.com>
2017-08-26 22:31:09 +0800

    index: Use dynamically rendered data

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const expressApp = express()
 app.prepare().then(() => {
   expressApp.get('/', (req, res) => app.render(req, res, '/'))
 
+  expressApp.get('/table', (req, res) => {
+    res.json([
+      {
+        video: 'videoL.mkv',
+        name: 'Great Eastern Highway',
+        date: '2017/07/27',
+        dist: '27.7km',
+        detected: 'sign',
+        probability: '0.83'
+      }
+    ])
+  })
+
+  expressApp.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
   expressApp.listen(3000, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,56 @@
+import React from 'react';
 import Layout from '../Layout'
+import { Table } from 'reactstrap'
 
-export default() => (
-    <div>
-        <Layout>
-            <div>Welcome to next.js</div>
-        </Layout>
-    </div>
-)
+class IndexPage extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            data: []
+        };
+    }
+
+    componentDidMount() {
+        console.log("Component did mount");
+        fetch('/table').then(d => d.json()).then(json => this.setState({
+            data: json
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <h1>Coconot - Detected Road Signs</h1>
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Video File</th>
+                                <th>Road Name</th>
+                                <th>Date</th>
+                                <th>Distance</th>
+                                <th>Detected Object</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.data.map(row => (
+                                 <tr>
+                                    <th scope="row">1</th>
+                                    <td>{row.video}</td>
+                                    <td>{row.name}</td>
+                                    <td>{row.date}</td>
+                                    <td>{row.dist}</td>
+                                    <td>{row.detected}</td>
+                                    <td>{row.probability}</td>
+                                 </tr>
+                             ))}
+                        </tbody>
+                    </Table>
+                </Layout>
+            </div>
+        )
+    }
+}
+
+export default IndexPage;
