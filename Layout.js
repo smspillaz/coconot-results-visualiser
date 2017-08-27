78331551e14ef23e4e656cd9be88c3fdf4431a97
Sam Spilsbury <smspillaz@gmail.com>
2017-08-27 10:26:18 +0800

    layout: Slight color bikeshed

--- Layout.js
+++ Layout.js
@@ -8,6 +8,12 @@ const Layout = (props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
     </Head>
+    <style jsx global>{`
+      body {
+        background: #f8f8f8
+      }
+    `}
+    </style>
     <Container>
       {props.children}
     </Container>
